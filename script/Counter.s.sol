7c211b12dc5752c6b8492dee3c1919ad18ad366d
Deeptha Parinda <74143503+DeepthaGit@users.noreply.github.com>
2025-07-13 16:46:02 +0530

    Update Counter.s.sol

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -1,31 +1,12 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.22;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
 
-contract Greeter {
-    string public name;
-    address public owner;
+import {Script, console} from "forge-std/Script.sol";
 
-    event NameChanged(string oldName, string newName);
+contract CounterScript is Script {
+    function setUp() public {}
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Only the owner can perform this action");
-        _;
-    }
-    
-    constructor(string memory _initialName) {
-        name = _initialName;
-        owner = msg.sender;
-    }
-
-
-    function changeName(string memory _newName) external onlyOwner {
-        string memory oldName = name;
-        name = _newName;
-        emit NameChanged(oldName, _newName);
-    }
-
-
-    function greet() external view returns (string memory) {
-        return string(abi.encodePacked("Hello, ", name, "!"));
+    function run() public {
+        vm.broadcast();
     }
 }
